7bc3ff7a72dd1958844d1623cf046eaaa587abc0
mehmeturmac <mehmeturmac@gmail.com>
2022-07-26 17:51:19 +0300

    css fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ function Header() {
   const dispatch = useDispatch();
 
   return (
-    <header className="header">
-      <h1 class="app-title">Markdown Previewer</h1>
-      <div class="help-button false" onClick={() => dispatch(setDefaultText())}>
-        <i class="fa fa-question" aria-hidden="true">
+    <header>
+      <h1 className="app-title">Markdown Previewer</h1>
+      <div className="help-button false" onClick={() => dispatch(setDefaultText())}>
+        <i className="fa fa-question" aria-hidden="true">
           ?
         </i>
       </div>
